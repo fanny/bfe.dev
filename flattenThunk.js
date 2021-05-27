e7eca3d9776f21eb12af5e5d0419c5fbb10702ff
Fanny <fannyvieira082@gmail.com>
2021-05-27 19:40:39 -0300

    Create flattenThunk.js

---
+++ flattenThunk.js
@@ -0,0 +1,33 @@
+
+/**
+ * @param {Thunk} thunk
+ * @return {Thunk}
+ */
+function flattenThunk(thunk) {
+  // your code here
+  return function (callback) {
+    const helper = (error, data) => {
+      if(!(data instanceof Function))
+        callback(error, data)
+      else
+        data(helper)
+    }
+    thunk(helper)
+  }
+}
+
+const func1 = (cb) => {
+  setTimeout(() => cb(null, 'ok'), 10)
+}
+
+const func2 = (cb) => {
+  setTimeout(() => cb(null, func1), 10)
+}
+
+const func3 = (cb) => {
+  setTimeout(() => cb(null, func2), 10)
+}
+
+flattenThunk(func3)((error, data) => {
+   console.log(data) // 'ok'
+})
