4627719f38b574c268c5ab6b099d1afba9d32914
Fanny <fannyvieira082@gmail.com>
2021-05-30 14:27:47 -0300

    Create memoization.js

---
+++ memoization.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} func
+ * @param {(args:[]) => string }  [resolver] - cache key generator
+ */
+function memo(func, resolver = (a, b) => [a, b].join('_')) {
+  const cache = {}
+  return function (...args) {
+    const key = resolver(...args)
+    if(cache[key]){
+      return cache[key]
+    } else {
+      cache[key] = func.apply(this, args)
+      return cache[key]
+    }
+  }
+}
